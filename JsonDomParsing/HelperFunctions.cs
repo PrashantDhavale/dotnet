3f4babab2557c409e4d8c5de43c9e94dd9d82c8e
Prashant Dhavale <pndhavale@gmail.com>
2023-06-17 22:31:12 +1000

    JsonDomParsing

---
+++ JsonDomParsing/HelperFunctions.cs
@@ -0,0 +1,58 @@
+﻿using System.Text.Json.Nodes;
+using System.Text.Json;
+
+namespace JsonDomParsing;
+
+public class HelperFunctions
+{
+    public static async Task<string?> ReadJsonFileAsync(string filePath)
+    {
+        using StreamReader sr = new StreamReader(filePath);
+        return await sr.ReadToEndAsync();
+    }
+
+    public static IEnumerable<string?> ReadJsonArrayElements(string? rawJson, string? nodePath = "")
+    {
+        var jsonNode = JsonNode.Parse(rawJson!)!;
+
+        string[] pathNodeNames = nodePath!.Split(".", StringSplitOptions.RemoveEmptyEntries);
+
+        var dataNode = jsonNode.Root;
+        foreach (var pathNode in pathNodeNames)
+            dataNode = dataNode![pathNode]!;
+
+        var arrayAsStrings = new List<string?>();
+        if (dataNode is not null && dataNode is JsonArray)
+        {
+            foreach (var item in dataNode.AsArray())
+            {
+                arrayAsStrings.Add(item!.ToJsonString());
+            }
+            return arrayAsStrings;
+        }
+        return Enumerable.Empty<string>();
+    }
+
+    public static IEnumerable<string?> ReadJsonArray(string? rawJson, string? nodePath = "")
+    {
+        using (JsonDocument document = JsonDocument.Parse(rawJson!))
+        {
+            var root = document.RootElement;
+            string[] pathNodeNames = nodePath!.Split(".", StringSplitOptions.RemoveEmptyEntries);
+
+            foreach (var pathNode in pathNodeNames)
+                root.TryGetProperty(pathNode, out root);
+
+            if (root.ValueKind is JsonValueKind.Array)
+            {
+                var arrayAsStrings = new List<string?>();
+                foreach (JsonElement item in root.EnumerateArray())
+                {
+                    arrayAsStrings.Add(item.GetRawText());
+                }
+                return arrayAsStrings;
+            }
+        }
+        return Enumerable.Empty<string>();
+    }
+}
---
+++ JsonDomParsing/Program.cs
@@ -0,0 +1,34 @@
+﻿namespace JsonDomParsing;
+
+public class Program
+{
+    public static async Task Main()
+    {
+        var rawPayload1 = await HelperFunctions.ReadJsonFileAsync("sample.json");
+        var rawPayload2 = await HelperFunctions.ReadJsonFileAsync("sampleArray.json");
+
+        var elements1 = HelperFunctions.ReadJsonArrayElements(rawPayload1, "data.balances");
+        foreach (var item in elements1)
+        {
+            Console.WriteLine($"JSON1={item}");
+        }
+
+        var elements2 = HelperFunctions.ReadJsonArray(rawPayload1, "data.balances");
+        foreach (var item in elements2)
+        {
+            Console.WriteLine($"JSON2={item}");
+        }
+
+        var elements3 = HelperFunctions.ReadJsonArrayElements(rawPayload2);
+        foreach (var item in elements3)
+        {
+            Console.WriteLine($"JSON3={item}");
+        }
+
+        var elements4 = HelperFunctions.ReadJsonArray(rawPayload2);
+        foreach (var item in elements4)
+        {
+            Console.WriteLine($"JSON4={item}");
+        }
+    }
+}
